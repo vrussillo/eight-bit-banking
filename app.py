5c00c443b5f54d235de627e3bc5671ac45826604
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-11-11 15:50:16 -0800

    db adjustments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from operator import add
 import os
+import re
 from flask import Flask, render_template, session, redirect, flash, g, request
 from flask_debugtoolbar import DebugToolbarExtension
 from forms import RegisterForm, LoginForm, AddCrypto, UserEditForm
@@ -13,7 +14,12 @@ CURR_USER_KEY = "user_id"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///eight_bit_banking_db')
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://eight_bit_banking_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get ('SECRET_KEY', 'billybobthorton1')
