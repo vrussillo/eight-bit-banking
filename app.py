8459645c6a55e434b0650eac5404366e49378cc0
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-11-11 14:02:47 -0800

    os for env

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///eight_bit_banking_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = os.environ.get ('SECRET_KEY', 'secret1')
+app.config['SECRET_KEY'] = os.environ.get ('SECRET_KEY', 'billybobthorton1')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
