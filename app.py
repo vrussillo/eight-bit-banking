6d57f14eff457ec49c3419efd423b2e5db087f64
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-11-11 16:07:06 -0800

    db adjustments

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ CURR_USER_KEY = "user_id"
 
 app = Flask(__name__)
 
-
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
